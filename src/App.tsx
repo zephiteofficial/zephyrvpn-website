1c221caa9f61996c50979c2376643f6b9d4f9159
AtikshKhirbat <atikshkhirbat0@gmail.com>
2025-02-22 02:02:31 +0530

    Real Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from './pages/home';
 import Nav from './components/nav';
 import Plans from './pages/plans';
+
 import Footer from "./components/footer";
+
+
 
 function App() {
   return (
-    <Router> {/* Use HashRouter */}
-      <div className="bg-background">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/plans" element={<Plans />} />
-        </Routes>
-        <div className="mt-24">
-          <Footer />
-        </div>
-      </div>
-    </Router>
-  );
+    <div className="bg-background">
+          <Nav />
+          <Routes>
+  
+            <Route path="/" element={<Home />} />
+            <Route path="/plans" element={<Plans />} />
+           
+          </Routes>
+          <div className="mt-24">
+            <Footer />
+          </div>
+    </div>
+    
+  )
 }
-
 export default App;
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
+    <HashRouter>
       <App />
+    </HashRouter>
   );
 } else {
   console.error('Failed to find the root element');
