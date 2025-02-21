eb57280c38fb24761276c56f68d2065a1da714db
AtikshKhirbat <atikshkhirbat0@gmail.com>
2025-02-22 01:40:33 +0530

    aosplidng0pawi

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/zephyrvpn-website/',  // Set this to your repository name
+  base: './', 
   plugins: [react()],
   build: {
     outDir: 'dist',
