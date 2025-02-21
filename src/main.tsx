87dc1ec044f2061d6e18504f13492efe4afc032a
AtikshKhirbat <atikshkhirbat0@gmail.com>
2025-02-22 01:54:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter } from 'react-router-dom';
-
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   );
 } else {
   console.error('Failed to find the root element');
