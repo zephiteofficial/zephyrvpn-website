585cb656adef3df9f33b35996ca0cbb0bdb799df
AtikshKhirbat <atikshkhirbat0@gmail.com>
2025-02-22 01:19:49 +0530

    Main Website 2nd fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/home';
 import Nav from './components/nav';
 import Plans from './pages/plans';
-
 import Footer from "./components/footer";
-
-
 
 function App() {
   return (
-    <div className="bg-background">
-          <Nav />
-          <Routes>
-  
-            <Route path="/" element={<Home />} />
-            <Route path="/plans" element={<Plans />} />
-           
-          </Routes>
-          <div className="mt-24">
-            <Footer />
-          </div>
-    </div>
-    
-  )
+    <Router> {/* Use HashRouter */}
+      <div className="bg-background">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/plans" element={<Plans />} />
+        </Routes>
+        <div className="mt-24">
+          <Footer />
+        </div>
+      </div>
+    </Router>
+  );
 }
-export default App;
 
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
-  base: "/zephyrvpn-website/"
+  base: '/zephyrvpn-website/',  // Set this to your repository name
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 });
